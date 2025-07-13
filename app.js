5fe7d033536ccb424fc948d3564df4f5c8f2d344
Mariana Abigail Céspedes <cespedesmarianaabigail@gmail.com>
2025-07-13 14:18:32 -0300

    App de Empleados

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
-let items =[];
+let empleados = [];
 
 const formulario = document.getElementById("formulario");
-const input = document.getElementById("input-texto");
 const lista = document.getElementById("lista");
 
-const mostrarLista = () => {
-    lista.innerHTML = items.map((item) => `<div class="tarjeta"> ${item} </div>`)
-    .join("")
+const nombreInput = document.getElementById("nombre");
+const dniInput = document.getElementById("dni");
+const cargoInput = document.getElementById("cargo");
 
+const mostrarLista = () => {
+  lista.innerHTML = empleados.map((empleado, index) => `
+    <div class="tarjeta">
+      <p><strong>Nombre:</strong> ${empleado.nombre}</p>
+      <p><strong>DNI:</strong> ${empleado.dni}</p>
+      <p><strong>Cargo:</strong> ${empleado.cargo}</p>
+      <button onclick="eliminarEmpleado(${index})">X</button>
+    </div>
+  `).join("");
 };
 
-formulario.addEventListener("submit", (e) => {  
-    e.preventDefault()
-    const texto = input.ariaValueMax.trim();
-    if (texto === "")return;
 
-    items.push(texto);
-    input.value = "";
-    mostrarLista();
+const eliminarEmpleado = (index) => {
+  empleados.splice(index, 1);
+  mostrarLista();
+};
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nombre = nombreInput.value.trim();
+  const dni = dniInput.value.trim();
+  const cargo = cargoInput.value.trim();
+
+  if (nombre === "" || dni === "") return;
+
+  const nuevoEmpleado = {
+    nombre,
+    dni,
+    cargo
+  };
+
+  empleados.push(nuevoEmpleado);
+
+  nombreInput.value = "";
+  dniInput.value = "";
+  cargoInput.value = "";
+
+  mostrarLista();
 });
+
